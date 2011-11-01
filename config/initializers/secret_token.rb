6fc899ac0b8ab5494173123054188d4cbee3e6b4
Bryan Jacobson <bryans.adventure@gmail.com>
2011-11-01 06:23:25 -0700

    Initial file.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learnshare::Application.config.secret_token = '264ef37ada7c4f5f25fb6d3f1d7f7c05aa80789e049ef1cafd01818da840ab683a0c5d87b3e2c1e092406e29bf1284e2a32cc9fc37abff22397b7654ca03f022'
