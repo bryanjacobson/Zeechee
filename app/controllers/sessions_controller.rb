b5ae1103ddb98d2f353ec1002a8466dd4e2821d5
Bryan Jacobson <bryans.adventure@gmail.com>
2011-12-10 13:55:39 -0800

    Don't list topics on home page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, :notice => "Logged in successfully"
+      redirect_to topics_path, :notice => "Logged in successfully"
     else
       redirect_to root_path, :notice => "Invalid login/password combination"
     end
