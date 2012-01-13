7c2e82ce7752b149b5efea33f262c71765ebcd03
Bryan Jacobson <bryans.adventure@gmail.com>
2012-01-12 19:47:27 -0800

    Item models auto-detect images!!

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ActiveRecord::Base
   belongs_to :uset
   belongs_to :screen
   acts_as_list :scope => :screen
+  before_save :auto_detect
 
   def item_types
     [ ["Auto Detect", 1],
@@ -9,4 +10,15 @@ class Item < ActiveRecord::Base
       ["Image", 3]
     ]
   end
+
+  private
+    def auto_detect
+      return if self.item_type_id != 1
+      s = self.body.downcase
+      if s =~ /\.jpg$/ || s =~ /\.jpeg$/ || s =~ /\.png$/ || s =~ /\.bmp$/ || s =~ /\.gif$/
+        self.item_type_id = 3
+        return
+      end
+      self.item_type_id = 2
+    end
 end
